604daea898a76f65a81a192e684aef3da1adfcec
Rahul Khanna <khanna.mailme@gmail.com>
2019-01-02 16:06:08 +0800

    fixing nonce issue

--- src/sso.js
+++ src/sso.js
@@ -7,6 +7,7 @@ const SSO = ((global, $, partnerKey) => {
   const authenticate = (queryParams) => {
     let astroIdentitySessionState = Utils.getQueryStringValue('session_state') || Utils.getCookie(CONFIG.COOKIE.SESSION) // Astro Identity Session State
     let astroNonce = Utils.getQueryStringValue('nonce') || Utils.getCookie(CONFIG.COOKIE.NONCE) // Astro Identity Nonce
+    debugger;
     if(astroIdentitySessionState) { // need to get user profile
       Utils.setCookie(CONFIG.COOKIE.SESSION, astroIdentitySessionState);
       if (!astroNonce) {
@@ -21,6 +22,8 @@ const SSO = ((global, $, partnerKey) => {
         global.dispatchEvent(new CustomEvent("authStatusChange")); 
       });
     } else if (!astroNonce) { // need to redirect to identity portal to check user auth state
+      astroNonce = Utils.generateNonce();
+      Utils.setCookie(CONFIG.COOKIE.NONCE, astroNonce);
       queryString = Utils.makeQueryString(queryParams);
       global.location.href = `${CONFIG.PORTAL.DOMAIN}/auth?${queryString}`;
     }
